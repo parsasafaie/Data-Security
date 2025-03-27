ce68281084f0006bd1bbd437d9decf93e56026cf
parsasafaie <parsasafaie.2568@proton.me>
2025-03-27 12:58:30 +0330

    update emogi_encryption function for ability of  decryption

--- encryption.py
+++ encryption.py
@@ -1,9 +1,9 @@
 from random import randint
 
 def emoji_encryption(text: str, emojies: list):
-    return ''.join(emojies[int(bit)][randint(0, 49)] for bit in ''.join(format(byte, '08b') for byte in text.encode()))
+    return ''.join(emojies[int(bit)][randint(0, 49)] for bit in '2'.join(format(byte, '08b') for byte in text.encode()))
 
 if __name__=="__main__":
     with open("files/emoji.txt", 'r', encoding='utf-8') as file:
         emojies = file.readlines()
-    print(emoji_encryption('Parsa', emojies=emojies))
+    print(emoji_encryption('Parsa Safaie', emojies=emojies))
