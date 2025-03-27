b365f8f0ff636abd5a036116eaafa24446afe9ca
parsasafaie <parsasafaie.2568@proton.me>
2025-03-27 12:45:19 +0330

    Init

---
+++ encryption.py
@@ -0,0 +1,9 @@
+from random import randint
+
+def emoji_encryption(text: str, emojies: list):
+    return ''.join(emojies[int(bit)][randint(0, 49)] for bit in ''.join(format(byte, '08b') for byte in text.encode()))
+
+if __name__=="__main__":
+    with open("files/emoji.txt", 'r', encoding='utf-8') as file:
+        emojies = file.readlines()
+    print(emoji_encryption('Parsa', emojies=emojies))
